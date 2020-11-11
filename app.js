bc7a560b0d29ff98db5970bc6ec43da5c8fa92cb
hdsenevi <shanaka36@gmail.com>
2020-11-11 21:05:42 +1100

    showing rssi

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const BeaconScanner = require('node-beacon-scanner');
 const scanner = new BeaconScanner();
-const BeaconMapper = require('./utils/beaconDataMapper.js');
+// const BeaconMapper = require('./utils/beaconDataMapper.js');
 
 // Set an Event handler for becons
 scanner.onadvertisement = (advertisement) => {
   if (advertisement.beaconType === 'iBeacon') {
-    const beacon = BeaconMapper(advertisement);
+    const beacon = beaconMapper(advertisement);
     console.log(JSON.stringify(beacon));
   }
 };
 
 // Start scanning
 scanner.startScan().then(() => {
-  console.log('Started to scan.')  ;
+  console.log('Started to scan.');
 }).catch((error) => {
   console.error(error);
 });
+
+const beaconMapper = (advertisement) => {
+  return {
+    "uuid": advertisement.iBeacon.uuid,
+    "major": advertisement.iBeacon.major,
+    "minor": advertisement.iBeacon.minor,
+    "txPower": advertisement.iBeacon.txPower,
+    "rssi": advertisement.rssi,
+  }
+}
